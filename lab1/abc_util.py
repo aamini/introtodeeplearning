0f50d00807f7f21d24acf468dff3276caca7aa07
Alexander Amini <xan.amini@gmail.com>
2019-01-21 01:04:22 -0500

    version 1

--- lab1/abc_util.py
+++ lab1/abc_util.py
@@ -4,20 +4,29 @@ import regex as re
 
 
 def extract_song_snippet(generated_text):
-    pattern = '\r\n\r\n(.*?)\r\n\r\n'
+    pattern = '\n\n(.*?)\n\n'
     search_results = re.findall(pattern, generated_text, overlapped=True, flags=re.DOTALL)
     songs = [song for song in search_results]
     return songs
 
 def save_song_to_abc(song, filename="tmp"):
-    with open("{}.abc".format(filename), "w") as f:
+    save_name = "{}.abc".format(filename)
+    with open(save_name, "w") as f:
         f.write(song)
+    return filename
 
 def abc2wav(abc_file):
-    path = os.path.realpath(__file__)
-    path_to_tool = '.'
-    subprocess.run([os.path.join(path_to_tool, "abc2wav"), abc_file])
+    path_to_tool = './introtodeeplearning_labs/lab1/abc2wav'
+    cmd = "{} {}".format(path_to_tool, abc_file)
+    return os.system(cmd)
 
-def extract_audio_snippet(generated_text):
-    song = extract_audio_snippet(generated_text)[0]
-    save_song_to_abc(song)
+def play_wav(wav_file):
+    from IPython.display import Audio
+    return Audio(wav_file)
+
+def play_generated_song(generated_text):
+    songs = extract_song_snippet(generated_text)
+    song = songs[0]
+    basename = save_song_to_abc(song)
+    abc2wav(basename+'.abc')
+    return play_wav(basename+'.wav')
