f5cf534bee7009547bd4a04c34751f2694f1db1c
Alexander Amini <xan.amini@gmail.com>
2019-01-24 14:27:59 -0500

    training loader added to lab2

--- lab2/util.py
+++ lab2/util.py
@@ -7,6 +7,51 @@ import time
 
 IM_SHAPE = (64, 64, 3)
 
+
+class TrainingDatasetLoader(object):
+    def __init__(self, data_path):
+
+        print "Opening {}".format(data_path)
+        sys.stdout.flush()
+
+        self.cache = h5py.File(data_path, 'r')
+
+        print "Loading data into memory..."
+        sys.stdout.flush()
+        self.images = self.cache['images'][:]
+        self.labels = self.cache['labels'][:]
+        self.image_dims = self.images.shape
+        n_train_samples = self.image_dims[0]
+
+        self.train_inds = np.random.permutation(np.arange(n_train_samples))
+
+        self.pos_train_inds = self.train_inds[ self.labels[self.train_inds, 0] == 1.0 ]
+        self.neg_train_inds = self.train_inds[ self.labels[self.train_inds, 0] != 1.0 ]
+
+    def get_train_size(self):
+        return self.train_inds.shape[0]
+
+    def get_batch(self, n, only_faces=False, p_pos=None, p_neg=None, return_inds=False):
+        if only_faces:
+            selected_inds = np.random.choice(self.pos_train_inds, size=n, replace=False, p=p_pos)
+        else:
+            selected_pos_inds = np.random.choice(self.pos_train_inds, size=n//2, replace=False, p=p_pos)
+            selected_neg_inds = np.random.choice(self.neg_train_inds, size=n//2, replace=False, p=p_neg)
+            selected_inds = np.concatenate((selected_pos_inds, selected_neg_inds))
+
+        sorted_inds = np.sort(selected_inds)
+        train_img = self.images[sorted_inds,:,:,::-1]/255.
+        train_label = self.labels[sorted_inds,...]
+        return (train_img, train_label, sorted_inds) if return_inds else (train_img, train_label)
+
+    def get_n_most_prob_faces(self, prob, n):
+        idx = np.argsort(prob)[::-1]
+        most_prob_inds = self.pos_train_inds[idx[:10*n:10]]
+        return self.images[most_prob_inds,...]/255.
+
+    def get_all_train_faces(self):
+        return self.images[ self.pos_train_inds ]
+
 class PPBFaceEvaluator:
     ''' Evaluate on the PPB dataset'''
     def __init__(self):
@@ -72,7 +117,7 @@ class PPBFaceEvaluator:
                    (not from_logit and most_likely_prob >= 0.5):
                         correct_predictions[model_idx] += 1
 
-        accuracy = [correct_predictions[i]/num_faces*100 for i,_ in enumerate(models_to_test)]
+        accuracy = [correct_predictions[i]/num_faces for i,_ in enumerate(models_to_test)]
         return accuracy
 
 
