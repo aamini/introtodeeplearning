341dfb66b7b2ccd06700e0772eb905402f51ae51
Alexander Amini <xan.amini@gmail.com>
2020-01-02 13:56:00 -0500

    adding lab1 tests

--- mitdeeplearning/lab1.py
+++ mitdeeplearning/lab1.py
@@ -29,8 +29,6 @@ def save_song_to_abc(song, filename="tmp"):
 
 def abc2wav(abc_file):
     path_to_tool = os.path.join(cwd, 'bin', 'abc2wav')
-    print("path_to_tool", path_to_tool)
-
     cmd = "{} {}".format(path_to_tool, abc_file)
     return os.system(cmd)
 
@@ -55,3 +53,26 @@ def play_generated_song(generated_text):
         play_song(song)
     print("None of the songs were valid, try training longer to improve \
         syntax.")
+
+def test_batch_func_types(func, args):
+    ret = func(*args)
+    assert len(ret) == 2, "[FAIL] get_batch must return two arguments (input and label)"
+    assert type(ret[0]) == np.ndarray, "[FAIL] test_batch_func_types: x is not np.array"
+    assert type(ret[1]) == np.ndarray, "[FAIL] test_batch_func_types: y is not np.array"
+    print("[PASS] test_batch_func_types")
+    return True
+
+def test_batch_func_shapes(func, args):
+    dataset, seq_length, batch_size = args
+    x, y = func(*args)
+    correct = (batch_size, seq_length)
+    assert x.shape == correct, "[FAIL] test_batch_func_shapes: x {} is not correct shape {}".format(x.shape, correct)
+    assert y.shape == correct, "[FAIL] test_batch_func_shapes: y {} is not correct shape {}".format(y.shape, correct)
+    print("[PASS] test_batch_func_shapes")
+    return True
+
+def test_batch_func_next_step(func, args):
+    x, y = func(*args)
+    assert (x[:,1:] == y[:,:-1]).all(), "[FAIL] test_batch_func_next_step: x_{t} must equal y_{t-1} for all t"
+    print("[PASS] test_batch_func_next_step")
+    return True
