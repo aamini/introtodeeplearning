6d0086e7bd0d227918903a96ab8508dd2c126bff
Alexander Amini <xan.amini@gmail.com>
2020-01-02 21:18:58 -0500

    mnist plottting

--- mitdeeplearning/lab2.py
+++ mitdeeplearning/lab2.py
@@ -9,6 +9,37 @@ import sys
 
 IM_SHAPE = (64, 64, 3)
 
+def plot_image_prediction(i, predictions_array, true_label, img):
+  predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+
+  plt.imshow(np.squeeze(img), cmap=plt.cm.binary)
+
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label:
+    color = 'blue'
+  else:
+    color = 'red'
+
+  plt.xlabel("{} {:2.0f}% ({})".format(predicted_label,
+                                100*np.max(predictions_array),
+                                true_label),
+                                color=color)
+
+def plot_value_prediction(i, predictions_array, true_label):
+  predictions_array, true_label = predictions_array[i], true_label[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+  thisplot = plt.bar(range(10), predictions_array, color="#777777")
+  plt.ylim([0, 1])
+  predicted_label = np.argmax(predictions_array)
+
+  thisplot[predicted_label].set_color('red')
+  thisplot[true_label].set_color('blue')
+
 
 class TrainingDatasetLoader(object):
     def __init__(self, data_path):
