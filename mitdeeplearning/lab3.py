e1aeb24552cc700f137fda5f0f778ee8c5a3838e
Alexander Amini <xan.amini@gmail.com>
2020-01-06 23:37:22 -0500

    typoo

--- mitdeeplearning/lab3.py
+++ mitdeeplearning/lab3.py
@@ -4,10 +4,18 @@ from IPython.display import HTML
 
 
 def play_video(filename):
-    encoded = base64.b64encode(io.open('./agent.mp4', 'r+b').read())
+    encoded = base64.b64encode(io.open(filename, 'r+b').read())
     embedded = HTML(data='''
         <video controls>
             <source src="data:video/mp4;base64,{0}" type="video/mp4" />
         </video>'''.format(encoded.decode('ascii')))
 
     return embedded
+
+def preprocess_pong(image):
+    I = image[35:195] # Crop
+    I = I[::2, ::2, 0] # Downsample width and height by a factor of 2
+    I[I == 144] = 0 # Remove background type 1
+    I[I == 109] = 0 # Remove background type 2
+    I[I != 0] = 1 # Set remaining elements (paddles, ball, etc.) to 1
+    return I.astype(np.float).reshape(80, 80, 1)
