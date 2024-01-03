5b48c67303b3c4f6e154417679ea6590543a9f8a
Divya Nori <divnor80@mit.edu>
2024-01-03 14:55:09 -0500

    part 1 code

--- xtra_labs/llm_finetune/draft.py
+++ xtra_labs/llm_finetune/draft.py
@@ -2,20 +2,86 @@
 Drafting lab flow in script format using PyTorch
 """
 
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import transformers
+
+from utils import run_benchmark
+
 # Part 1
 
+# TEXT: overview of LLM lab
 # Load pretrained LLM (medium size model)
 
-# Function for next token prediction
+model_name = "facebook/opt-1.3b" 
+model = transformers.TFAutoModelForCausalLM.from_pretrained(model_name)
+tokenizer = transformers.AutoTokenizer.from_pretrained(model_name)
 
+# TEXT: explain tokenizer
+# Include cell for tokenizer inspection
+
+# TEXT: explain how LLMs are trained for next token prediction 
+# Write a function to predict next token
+
+def predict_next_token(probs):
+    new_token = np.random.choice(len(probs), p=probs.numpy())
+    print(tokenizer.decode(new_token), end='', flush=True)
+    return new_token
+
+# TEXT: explain that next token prediction must be called multiple times for inference
 # Call in loop for autoregressive inference
 
-# Evaluate model on benchmark dataset
+def generate(start_text, num_steps=20, temp=1.): 
+    print(start_text, end="")
+    x = tokenizer.encode(start_text)
+    num_start = len(x)
 
-# Do the same for small and large models
+    for i in range(num_steps):
+        input_tensor = tf.reshape(tf.constant(x), [1, -1])
+        logits = model(input_tensor).logits
+        probs = tf.nn.softmax(logits/temp)[0, -1, :]
+
+        new_token = predict_next_token(probs)
+        x.append(new_token)
+    
+    output = tokenizer.decode(x[num_start:])
+    return output
+
+# Test autoregressive generation
+
+# while True: 
+#     print("\n\n\n\n\n")
+#     input_text = input("Prompt: ")
+#     output = generate(input_text)
+
+# TEXT: some background on LLM benchmarking
+# Load benchmark dataset and evaluate model
+
+dataset = pd.read_csv("benchmark.csv")
+category_accs_1300m, avg_acc_1300m = run_benchmark(model, tokenizer)
+
+# TEXT: ask them to make a prediction on how accuracy will be affected by different model sizes
+
+# Benchmark smaller model
+
+model_name_350m = "facebook/opt-350m" 
+model_350m = transformers.TFAutoModelForCausalLM.from_pretrained(model_name_350m)
+tokenizer_350m = transformers.AutoTokenizer.from_pretrained(model_350m)
+
+category_accs_350m, avg_acc_350m = run_benchmark(model_350m, tokenizer_350m)
+
+# Benchmark larger model
+
+model_name_2700m = "facebook/opt-2.7b" 
+model_2700m = transformers.TFAutoModelForCausalLM.from_pretrained(model_name_2700m)
+tokenizer_2700m = transformers.AutoTokenizer.from_pretrained(model_2700m)
+
+category_accs_2700m, avg_acc_2700m = run_benchmark(model_2700m, tokenizer_2700m)
 
 # Spider plot
 
+print(category_accs_1300m)
 
 # Part 2
 
